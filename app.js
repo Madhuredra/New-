dd46df5a82162c404a872f12ed917a5ef8bd6d00
Madhurendra Nath Tiwari <madhurendraakela123@gmail.com>
2022-01-14 02:06:59 +0530

    second commit

--- app.js
+++ app.js
@@ -24,3 +24,50 @@ function onmouseMoveHobverChangeEmoticons(){
   document.getElementById("emoji").innerText = "🙂";
   
 }
+
+function ShowMe(event){
+  if(event==="btn1"){
+    // console.log(document.getElementById("show1"))
+    document.getElementById("show1").style.zIndex=1;
+    document.getElementById("show2").style.zIndex=0;
+    // document.getElementById("show2").style.visibility = hidden;
+    document.getElementById("show3").style.zIndex=0;
+    // document.getElementById("show3").style.visibility = hidden;
+  } 
+  if(event==="btn2"){
+    document.getElementById("show1").style.zIndex=0;
+    document.getElementById("show2").style.zIndex=1;
+    document.getElementById("show3").style.zIndex=0;
+  } 
+  if(event=="btn3"){
+    document.getElementById("show1").style.zIndex=0;
+    document.getElementById("show2").style.zIndex=0;
+    document.getElementById("show3").style.zIndex=1;
+  }                                                                     
+} 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
